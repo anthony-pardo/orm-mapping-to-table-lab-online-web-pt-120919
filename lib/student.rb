202eca96c9b7cf768c0f78b5fe3872efc4d9f3ab
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-25 19:08:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,11 +34,15 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def save 
-    sql =  <<-SQL 
-      DROP TABLE students
-      SQL
-      
-    DB[:conn].execute(sql)
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+ 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+ 
   end
 end
